341ca1a6269a18036f6637cdbda54fed8de4cd14
Vaishnav Sandeep <90813931+itsmehecker@users.noreply.github.com>
2025-03-15 09:23:47 +0000

    windows start ps1

---
+++ start_windows.ps1
@@ -0,0 +1,23 @@
+
+$DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$zfindScript = @"
+cd $DIR
+python -m venv "$DIR\venv"
+& "$DIR\venv\Scripts\Activate.ps1"
+pip install -r "$DIR\requirements.txt"
+python "$DIR\main.py"
+"@
+
+$zfindScript | Out-File -FilePath "$HOME\.zfind.ps1" -Encoding utf8
+
+$env:Path += ";$HOME"
+
+$shortcutPath = "$HOME\Desktop\zfind.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$shortcut = $WScriptShell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-File `"$HOME\.zfind.ps1`""
+$shortcut.Save()
+
+Write-Output "zfind installed successfully"
