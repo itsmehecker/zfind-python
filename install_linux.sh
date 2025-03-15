51c786af485cf7cd8aab1c1750d601df3cace0d3
Vaishnav Sandeep <90813931+itsmehecker@users.noreply.github.com>
2025-03-15 14:48:45 +0530

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -4,9 +4,5 @@ sudo apt update
 sudo apt install python3-full
 git clone https://github.com/itsmehecker/zfind-python.git
 cd zfind-python
-echo "Creating virtual Environment"
-python3 -m venv venv
-. venv/bin/activate
-pip install -r requirements.txt
 chmod +x start.sh
 sudo ./start.sh
