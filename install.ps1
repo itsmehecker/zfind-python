00702e86df6a3ef1825c5cb3c9a71b2bce135dfa
Vaishnav Sandeep <90813931+itsmehecker@users.noreply.github.com>
2025-03-15 14:54:25 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,4 @@
 git clone https://github.com/itsmehecker/zfind-python.git
 cd zfind-python
 echo "Creating virtual Environment"
-python3 -m venv venv
-. venv/bin/activate
-pip install -r requirements.txt
+./start_windows.ps1
