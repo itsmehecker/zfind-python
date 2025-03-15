90e841f1087ccbe45696d32c366d630c1dd7d34e
Vaishnav Sandeep <90813931+itsmehecker@users.noreply.github.com>
2025-03-15 14:48:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 git clone https://github.com/itsmehecker/zfind-python.git
 cd zfind-python
 echo "Creating virtual Environment"
-python3 -m venv venv
-. venv/Scripts/activate
-pip install -r requirements.txt
 chmod +x start.sh
 sudo ./start.sh
